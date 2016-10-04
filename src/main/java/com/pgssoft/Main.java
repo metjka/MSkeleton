861328b2b980548ae634463523afe198a2db44c4
Ihor Salnikov <isalnikov@pgs-soft.com>
2016-10-04 13:24:57 +0200

    add single main class

---
+++ src/main/java/com/pgssoft/Main.java
@@ -0,0 +1,17 @@
+package com.pgssoft;
+
+import com.pgssoft.first.MSkeletonApplication;
+import com.pgssoft.sec.SecService;
+
+/**
+ * Created by isalnikov on 10/4/2016.
+ * Time 1:13 PM
+ * MSkeleton
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SecService.main(args);
+        MSkeletonApplication.main(args);
+    }
+}
