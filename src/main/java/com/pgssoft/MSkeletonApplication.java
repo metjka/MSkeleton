5321080a9d9eb3219d07b3e124bece856a5e6c73
Ihor Salnikov <isalnikov@pgs-soft.com>
2016-10-04 11:18:38 +0200

    changed server.port to 8088 added test controller added spring-boot-starter-web

--- src/main/java/com/pgssoft/MSkeletonApplication.java
+++ src/main/java/com/pgssoft/MSkeletonApplication.java
@@ -9,4 +9,5 @@ public class MSkeletonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MSkeletonApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/pgssoft/TestController.java
@@ -0,0 +1,19 @@
+package com.pgssoft;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by isalnikov on 10/4/2016.
+ * Time 11:10 AM
+ * MSkeleton
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "test";
+    }
+
+}
