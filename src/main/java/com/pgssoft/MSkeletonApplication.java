e58bb14d443cf425a1f90e293dba3abe44c42724
Ihor Salnikov <isalnikov@pgs-soft.com>
2016-10-04 10:59:54 +0200

    Initial commit

---
+++ src/main/java/com/pgssoft/MSkeletonApplication.java
@@ -0,0 +1,12 @@
+package com.pgssoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSkeletonApplication.class, args);
+	}
+}
